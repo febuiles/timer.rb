bc51ce6d2ee3eaecf7062b00d48757b26c26b123
Federico Builes <federico.builes@gmail.com>
2010-12-06 19:09:19 -0500

    Adding TimeParser.

--- lib/timer/time_parser.rb
+++ lib/timer/time_parser.rb
@@ -1,7 +1,26 @@
 module Timer
   module TimeParser
     def self.parse_duration(duration)
-      nil
+      return nil if duration.nil?
+
+      total = 0
+      hours = []
+      mins = []
+      secs = []
+
+      time_chunks = duration.split(" ")
+      time_chunks.reject! { |c| c !~ /\d+[mhs]$/ }
+      hours += time_chunks.select {|c| c[-1,1] == "h" }
+      mins += time_chunks.select {|c| c[-1,1] == "m" }
+      secs += time_chunks.select {|c| c[-1,1] == "s" }
+
+      total += hours.inject(0) { |sum, x| sum + (x.to_i * 3600) }
+      total += mins.inject(0) { |sum, x| sum + (x.to_i * 60) }
+      total += secs.inject(0) { |sum, x| sum + x.to_i }
+
+      total unless total == 0
     end
   end
 end
+
+
